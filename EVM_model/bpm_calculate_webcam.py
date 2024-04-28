6985199247152596416d0563d4408c31bad62999
dmahesh123 <110253937+dmahesh123@users.noreply.github.com>
2024-04-28 01:19:17 -0500

    Comments Update bpm_calculate_webcam.py

--- EVM_model/bpm_calculate_webcam.py
+++ EVM_model/bpm_calculate_webcam.py
@@ -1,12 +1,20 @@
+# numerical operations
 import numpy as np
+# computer vision
 import cv2
+# system-specific parameters and functions
 import sys
-from cvzone.FaceDetectionModule import FaceDetector
-from cvzone.PlotModule import LivePlot
 import cvzone
+# FaceDetector from cvzone.FaceDetectionModule for face detection.
+from cvzone.FaceDetectionModule import FaceDetector
+# LivePlot from cvzone.PlotModule for live plotting
+from cvzone.PlotModule import LivePlot
+#  for time-related functions
 import time
+# for reading and writing CSV files
 import csv
 
+# Constants for video dimensions and parameters
 realWidth = 640   
 realHeight = 480  
 videoWidth = 160
@@ -14,9 +22,10 @@ videoHeight = 120
 videoChannels = 3
 videoFrameRate = 15
 
+# Initialize face detector and BPM values list
 detector = FaceDetector()
-bpm_values = []  # List to store BPM values
-
+# List to store BPM values
+bpm_values = []  
 
 # Color Magnification Parameters
 levels = 3
@@ -26,16 +35,18 @@ maxFrequency = 2.0
 bufferSize = 150
 bufferIndex = 0
 
+# initialize live plot for BPM visulaization
 plotY = LivePlot(realWidth,realHeight,[60,120],invert=True)
 
 # Helper Methods
+#construct Gaussian pyramid for the given frame
 def buildGauss(frame, levels):
     pyramid = [frame]
     for level in range(levels):
         frame = cv2.pyrDown(frame)
         pyramid.append(frame)
     return pyramid
-
+# Reconstruct frame from the Gaussian pyramid
 def reconstructFrame(pyramid, index, levels):
     filteredFrame = pyramid[index]
     for level in range(levels):
@@ -50,6 +61,7 @@ loadingTextLocation = (30, 40)
 bpmTextLocation = (videoWidth//2, 40)
 fpsTextLoaction = (500,600)
 
+# initialize graph items
 fontScale = 1
 fontColor = (255,255,255)
 lineType = 2
@@ -72,6 +84,7 @@ bpmBufferIndex = 0
 bpmBufferSize = 10
 bpmBuffer = np.zeros((bpmBufferSize))
 
+# variables for timing
 i = 0
 ptime = 0
 ftime = 0
@@ -79,39 +92,46 @@ ftime = 0
 # REPLACE WITH YOUR VIDEO PATH !!!!!
 video_path = "C:/Users/vishn/OneDrive/Desktop/DATASETS!!!/With_HR_precise_sync/Driver 4/session_1636641409569/video_1636641427815_88398/video_1636641427815_88398.mp4"
 
+# open video filr
 video = cv2.VideoCapture(video_path)
 if not video.isOpened():
     print("Error: Could not open video file.")
     sys.exit()
-
+    
+# Main loop for processing video frames
 while (True):
+    # Read a frame from the video
     ret, frame = video.read()
     if ret == False:
         break
+    # Resize frame to desired dimensions
     frame = cv2.resize(frame, (realWidth, realHeight))
        
-
+    # Detect faces in the frame
     frame, bboxs = detector.findFaces(frame,draw=False)
     frameDraw = frame.copy()
+
+    # Calculate frames per second (FPS)
     ftime = time.time()
     fps = 1 / (ftime - ptime)
     ptime = ftime
-
+    # Display FPS on the frame
     cv2.putText(frameDraw, f'FPS: {int(fps)}', (30, 440), 0, 1, [0, 0, 0], thickness=2, lineType=cv2.LINE_AA)
+    # If faces are detected
     if bboxs:
         x1, y1, w1, h1 = bboxs[0]['bbox']
         cv2.rectangle(frameDraw, bboxs[0]['bbox'], (255, 0, 255), 2)
         detectionFrame = frame[y1:y1 + h1, x1:x1 + w1]
         detectionFrame = cv2.resize(detectionFrame,(videoWidth,videoHeight))
 
-        # Construct Gaussian Pyramid
+        # Construct Gaussian Pyramid for the detected face
         videoGauss[bufferIndex] = buildGauss(detectionFrame, levels+1)[levels]
         fourierTransform = np.fft.fft(videoGauss, axis=0)
 
-        # Bandpass Filter
+        # Apply bandpass filter to the Fourier transform
         fourierTransform[mask == False] = 0
 
-        # Grab a Pulse
+        # Extract heart rate information
         if bufferIndex % bpmCalculationFrequency == 0:
             i = i + 1
             for buf in range(bufferSize):
@@ -122,7 +142,7 @@ while (True):
             bpmBufferIndex = (bpmBufferIndex + 1) % bpmBufferSize
             bpm_values.append(bpm)
 
-        # Amplify
+        # Amplify the signal
         filtered = np.real(np.fft.ifft(fourierTransform, axis=0))
         filtered = filtered * alpha
 
@@ -137,12 +157,12 @@ while (True):
 
         bpm_value = bpmBuffer.mean()
         imgPlot = plotY.update(float(bpm_value))
-
+        # Display BPM on the frame
         if i > bpmBufferSize:
             cvzone.putTextRect(frameDraw,f'BPM: {bpm_value}',bpmTextLocation,scale=2)
         else:
             cvzone.putTextRect(frameDraw, "Calculating BPM...", loadingTextLocation,scale=2)
-
+        # Display frame with BPM visualization
         if len(sys.argv) != 2:
             imgStack = cvzone.stackImages([frameDraw,imgPlot],2,1)
             cv2.imshow("Heart Rate Monitor", imgStack)
@@ -152,13 +172,13 @@ while (True):
     else:
         imgStack = cvzone.stackImages([frameDraw, frameDraw], 2, 1)
         cv2.imshow("Heart Rate Monitor", imgStack)
-
+# Write BPM values to a CSV file
 with open('bpm_values.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(['BPM'])
     for value in bpm_values:
         writer.writerow([value])
-
+# Clean up
 cv2.destroyAllWindows()
 
 
